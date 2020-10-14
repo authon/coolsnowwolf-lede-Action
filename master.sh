de499818f5056f8730ee320371bb58207acb2bbf
authon <authon@vip.qq.com>
2020-10-14 10:30:29 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -3,9 +3,12 @@
 # svn co https://github.com/coolsnowwolf/lede/trunk ./
 # rm -rf .svn
 
+rm -f ./
 git clone https://github.com/coolsnowwolf/lede.git
 mv -f lede/* .
 rm -rf lede
+
+# rsync -a lede/* .
 
 rm -rf ./*/.git
 rm -rf ./*/.svn
