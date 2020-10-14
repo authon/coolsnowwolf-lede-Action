d351ecb62226cb2ed2c426cb9a268c9d0af91706
authon <authon@vip.qq.com>
2020-10-14 09:38:23 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-svn co https://github.com/coolsnowwolf/lede/trunk ./
-rm -rf .svn
+# svn co https://github.com/coolsnowwolf/lede/trunk ./
+# rm -rf .svn
+
+rm -rf luci-app-openclash
+rm -rf OpenClash
+git clone https://github.com/coolsnowwolf/lede.git
+mv -f lede/ .
+rm -rf lede
 
 rm -rf ./*/.git
 rm -rf ./*/.svn
